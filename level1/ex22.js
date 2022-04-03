b7cae8b6e59e90f2563fbc4562a85e33c635524e
sieukim <s2lver@naver.com>
2022-04-04 04:39:37 +0900

    [level 1] 자연수 뒤집어 배열로 만들기(javascript)

--- level1/ex22.js
+++ level1/ex22.js
@@ -1,11 +1,3 @@
 function solution(n) {
-  // 주어진 숫자 -> 문자열 -> 배열
-  const answer = n.toString().split("");
-
-  // 각 요소: 문자열 -> 숫자
-  for (let i = 0; i < answer.length; i++) {
-    answer[i] = parseInt(answer[i]);
-  }
-
-  return answer.reverse();
+  return n.toString().split('').reverse().map(value => parseInt(value));
 }
