788cc086e8f0afe92d9877539753ffc768fe9813
sieukim <s2lver@naver.com>
2022-04-04 04:07:32 +0900

    [level 1] 문자열 내 p와 y의 개수(javascript)

--- level1/ex42.js
+++ level1/ex42.js
@@ -1,14 +1,15 @@
 function solution(s){
   let count_p = 0;
   let count_y = 0;
-
+  
+  // 소문자 문자열로 변환
+  s = s.toLowerCase();
+  
   // p와 y의 개수 세기
   s.split('').forEach(alpha => {
-    if (alpha.toLowerCase() === 'p') count_p++;
-    if (alpha.toLowerCase() === 'y') count_y++;
+      if (alpha === 'p') count_p++;
+      if (alpha === 'y') count_y++;
   })
-
-  if (count_p === count_y) return true;
-  else return false;
-
+  
+  return count_p === count_y
 }
