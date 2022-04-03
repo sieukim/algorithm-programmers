248c84e56d2d7541ddbf67a17fb53208d99ea2b8
sieukim <s2lver@naver.com>
2022-04-04 05:06:46 +0900

    [level 1] 최대공약수와 최소공배수(javascript)

--- level1/ex27.js
+++ level1/ex27.js
@@ -1,52 +1,52 @@
 function solution(n, m) {
   const answer = [];
-
+  
   // 최대 공약수 구하기
-  function maximumDivisor(n, m) {
-    let nIndex = n;
-    let mIndex = m;
-
-    while (nIndex > 0 && mIndex > 0) {
-      while (n % nIndex !== 0) {
-        nIndex--;
+  function gcd(n, m) {
+      let nIndex = n;
+      let mIndex = m;
+      
+      while (nIndex > 0 && mIndex > 0) {
+          while (n % nIndex !== 0) {
+              nIndex--;
+          }
+          
+          while (m % mIndex !== 0) {
+              mIndex--;
+          }
+          
+          if (nIndex === mIndex) {
+              break;
+          }
+          
+          if (nIndex > mIndex){
+              nIndex--;
+          } else {
+              mIndex--;
+          }
       }
-
-      while (m % mIndex !== 0) {
-        mIndex--;
-      }
-
-      if (nIndex === mIndex) {
-        break;
-      }
-
-      if (nIndex > mIndex){
-        nIndex--;
-      } else {
-        mIndex--;
-      }
-    }
-
-    return nIndex;
+      
+      return nIndex;
   }
-
+  
   // 최소 공배수 구하기
-  function minimumMultiple(n, m) {
-    let nIndex = 1;
-    let mIndex = 1;
-
-    while (n * nIndex !== m * mIndex){
-      if (n * nIndex > m * mIndex) {
-        mIndex++;
-      } else {
-        nIndex++;
+  function lcm(n, m) {
+      let nIndex = 1;
+      let mIndex = 1;
+      
+      while (n * nIndex !== m * mIndex){
+          if (n * nIndex > m * mIndex) {
+              mIndex++;
+          } else {
+              nIndex++;
+          }
       }
-    }
-
-    return n * nIndex;
+      
+      return n * nIndex;
   }
-
-  answer.push(maximumDivisor(n, m));
-  answer.push(minimumMultiple(n, m));
-
+  
+  answer.push(gcd(n, m));
+  answer.push(lcm(n, m));
+  
   return answer;
 }
