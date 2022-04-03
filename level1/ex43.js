e60a0cbbd41bbccc9cebfd842818680cdd0c7149
sieukim <s2lver@naver.com>
2022-04-04 04:15:08 +0900

    [level 1] 문자열 다루기 기본(javascript)

--- level1/ex43.js
+++ level1/ex43.js
@@ -1,14 +1,15 @@
 function solution(s) {
   // 숫자로만 구성된 문자열인지 확인하는 함수
   function isNumber(s) {
-    let answer = true;
-
-    s.split('').forEach(value => {
-      if (isNaN(value)) answer = false;
-    });
-
-    return answer;
+      for (let i = 0; i < s.length; i++) {
+          // 숫자가 아닌 경우
+          if (isNaN(s[i])) {
+              return false;
+          }
+      }
+      
+      return true;
   }
-
+  
   return (s.length === 4 || s.length === 6) && isNumber(s);
 }
