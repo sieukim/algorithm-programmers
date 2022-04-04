48917dfe8bc1516a0022188b503047e801f6f00f
sieukim <s2lver@naver.com>
2022-04-05 04:09:46 +0900

    [level 1] 3진법 뒤집기(javascript)

--- level1/ex17.js
+++ level1/ex17.js
@@ -1,31 +1,7 @@
 function solution(n) {
-  // 10진법에서 3진법으로 변환 후 뒤집는 함수
-  function toReversedTernary(number) {
-    let answer = '';
-
-    while (number > 0) {
-      answer += number % 3;
-      number = parseInt(number / 3);
-    }
-
-    return answer;
-  }
-
-  // 3진법에서 10진법으로 변환하는 함수
-  function toDecimal(str) {
-    let answer = 0;
-
-    const n = str.length;
-
-    for (let i = 0; i < n; i++) {
-      answer += str[i] * Math.pow(3, n - i - 1);
-    }
-
-    return answer;
-  }
-
-  const ternaryStr = toReversedTernary(n).toString();
-  const decimalNumber = toDecimal(ternaryStr);
-
-  return decimalNumber;
+  // 3진법 변환
+  const ternary = n.toString(3);
+  // 10진법 변환(3진법 뒤집음)
+  const decimal = parseInt(ternary.split('').reverse().join(''), 3);
+  return decimal;
 }
