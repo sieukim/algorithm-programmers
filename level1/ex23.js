f5d261f1d9bffca42d4db5a622f0789b6f6a78fc
sieukim <s2lver@naver.com>
2022-04-05 20:14:24 +0900

    [level 1] 정수 내림차순으로 배치하기(javascript)

--- level1/ex23.js
+++ level1/ex23.js
@@ -1,9 +1,3 @@
 function solution(n) {
-  // 주어진 숫자 -> 문자열 -> 배열
-  const answer = n.toString().split('');
-
-  // 내림차순 정렬
-  answer.sort((a, b) => b - a)
-
-  return parseInt(answer.join(''));
+  return parseInt(n.toString().split('').sort((a, b) => b - a).join(''));
 }
