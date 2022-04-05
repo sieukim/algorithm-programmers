d188f609910c5291b1e73e10ba5f5433b9e48e56
sieukim <s2lver@naver.com>
2022-04-05 20:42:46 +0900

    [level 1] 모의고사(javascript)

--- level1/ex12.js
+++ level1/ex12.js
@@ -1,40 +1,61 @@
 function solution(answers) {
+  // 점수 반환 함수
+  function getScore(answers, pattern) {
+      // 점수
+      let score = 0;
+      // 패턴 인덱스
+      let index = 0;
+      
+      for (let i = 0; i < answers.length; i++) {
+          // 정답인 경우
+          if (answers[i] === pattern[index]) {
+              score++;
+          }
+          
+          // 패턴 인덱스 갱신
+          index = (index + 1) % pattern.length;
+      }
+      
+      return score;
+  }
+  
+  // 수포자 셋..
+  const students = {
+      student1: {
+          pattern: [1, 2, 3, 4, 5],
+          score: 0,
+          number: 1,
+      }, 
+      student2: {
+          pattern: [2, 1, 2, 3, 2, 4, 2, 5],
+          score: 0,
+          number: 2,
+      },
+      student3: {
+          pattern: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+          score: 0,
+          number: 3,
+      },
+  }
+
+  // 최대 점수 
+  let maxScore = 0;
+  
+  // 점수 계산
+  for (const student in students) {
+      students[student].score = getScore(answers, students[student].pattern);
+      maxScore = Math.max(maxScore, students[student].score);
+  }
+
+  // 최대 점수 수포자 배열
   const answer = [];
-
-  // 수포자가 찍는 방식
-  const student1 = [1, 2, 3, 4, 5];
-  const student2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-  // 점수 계산 함수
-  function calculateScore(answers, student) {
-    // 수포자가 찍는 방식 패턴의 길이와 정답의 길이가 다르므로 별개의 인덱스를 둔다.
-    let studentIndex = 0;
-    // 수포자 점수
-    let studentScore = 0;
-
-    // 서로 비교하여 맞는 개수를 센다.
-    for (let i = 0; i < answers.length; i++) {
-      if (answers[i] === student[studentIndex]) studentScore++;
-      if (studentIndex + 1 === student.length) studentIndex = 0;
-      else studentIndex++;
-    }
-    return studentScore;
+  
+  for (const student in students) {
+      // 최대 점수인 경우
+      if (students[student].score === maxScore) {
+          answer.push(students[student].number);
+      }
   }
-
-  const studentsScore = [];
-
-  studentsScore.push(calculateScore(answers, student1));
-  studentsScore.push(calculateScore(answers, student2));
-  studentsScore.push(calculateScore(answers, student3));
-
-  // 점수 최대값
-  const maxScore = studentsScore.reduce((a, b) => Math.max(a, b));
-
-  // 점수 최대값과 같은 학생을 정답 배열에 푸쉬
-  for (let i = 0; i < studentsScore.length; i++) {
-    if (studentsScore[i] === maxScore) answer.push(i + 1);
-  }
-
+  
   return answer;
 }
