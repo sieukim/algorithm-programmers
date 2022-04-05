b8449fe54f5caa7be75ea5ac16e163c2a4966992
sieukim <s2lver@naver.com>
2022-04-05 20:04:10 +0900

    [level 1] k번째수(javascript)

--- level1/ex11.js
+++ level1/ex11.js
@@ -1,12 +1,11 @@
 function solution(array, commands) {
   const answer = [];
-
+  
   commands.forEach(command => {
-    let newArray = [];
-    newArray = array.slice(command[0] - 1, command[1]);
-    newArray.sort((a, b) => a - b);
-    answer.push(newArray[command[2] - 1]);
-  })
+      const [i, j, k] = command;
+      const number = array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+      answer.push(number);
+  });
 
   return answer;
 }
