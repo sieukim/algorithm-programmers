9aee6581b921f8a8a3e2581d13f9b154a21cdc73
sieukim <s2lver@naver.com>
2022-04-19 07:40:40 +0900

    [level 1] 비밀 지도(javascript)

--- level1/ex49.js
+++ level1/ex49.js
@@ -1,54 +1,39 @@
 function solution(n, arr1, arr2) {
-  // 이진수 배열 변환 함수
-  function toBinaryArr(number) {
-    const answer = [];
-
-    while (number > 0) {
-      answer.push(number % 2);
-      number = parseInt(number / 2);
-    }
-
-    return answer.reverse();
-  }
-
   // 맵 변환 함수
   function toMap(n, arr) {
-    const answer = Array.from(Array(n), () => Array(n).fill(''));
-
-    for (let i = 0; i < n; i++) {
-      const column = toBinaryArr(arr[i]);
-
-      while (column.length < n) {
-        column.unshift(0);
-      }
-
-      for (let j = 0; j < n; j++) {
-        answer[i][j] = column[j] === 0 ? ' ' : '#';
-      }
-    }
-
-    return answer;
+      return arr.map(row => {
+          // 이진수 배열로 변환
+          const binary = row.toString(2).split('');
+          // 길이가 n이 될 때까지 맨 앞에 0 추가
+          while (binary.length < n) {
+              binary.unshift(0);
+          }
+          // 길이가 n인 이진수 배열 반환
+          return binary;
+      });
   }
-
+  
   const map1 = toMap(n, arr1);
   const map2 = toMap(n, arr2);
 
   const map = [];
-
-  // 두 map 비교
+  
   for (let i = 0; i < n; i++) {
-    let column = '';
-
-    for (let j = 0; j < n; j++) {
-      if (map1[i][j] === '#' || map2[i][j] === '#') {
-        column += '#';
-      } else {
-        column += ' ';
+      let row = '';
+      
+      for (let j = 0; j < n; j++) {
+          // 둘 중 하나가 1(#)인 경우
+          if (map1[i][j] === '1' || map2[i][j] === '1') {
+              // # 추가
+              row += '#';
+          }
+          // 둘 다 1(#)이 아닌 경우
+          else {
+              row += ' ';
+          }
       }
-    }
-
-    map[i] = column;
+      map.push(row);
   }
-
+  
   return map;
 }
