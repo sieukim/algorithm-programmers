980a2350221bc017e08fe25ceb112cdd98aacef2
sieukim <s2lver@naver.com>
2022-04-24 19:57:50 +0900

    [level 1] 실패율(javascript)

--- level1/ex15.js
+++ level1/ex15.js
@@ -1,42 +1,33 @@
 function solution(N, stages) {
-  const answer = [];
-
-  const players = [];
-
-  for (let i = 0; i < N + 1; i++) {
-    players.push({level: i + 1, reached: 0, solved: 0});
+  // i번 스테이지 실패율 반환 함수
+  function getFailureRate(reached, i) {
+      // i번 스테이지 도달 && 클리어 x 사용자 수
+      const failurePlayers = reached[i];
+      // i번 스테이지 도달 사용자 수
+      const reachedPlayers = reached.slice(i).reduce((a, b) => a + b);
+      
+      // 도달 유저가 없는 경우
+      if (reachedPlayers === 0) {
+          // 실패율 = 0
+          return 0;
+      }
+      
+      return failurePlayers / reachedPlayers;
   }
+  
+  // reached[i]: i번 스테이지 도달 사용자 수
+  // reached[0] = 0, reached[N + 1] = 모든 스테이지 클리어한 사용자 수
+  const reached = new Array(N + 2).fill(0);
+  
+  // reached 초기화
+  stages.forEach(value => reached[value]++);
+  
+  // [스테이지 번호, 실패율] 배열, 0번과 N + 1번 제거
+  const info = reached.map((_, index) => [index, getFailureRate(reached, index)]).slice(1, N + 1);
+  
+  // 실패율을 기준으로 내림차순 정렬
+  info.sort((a, b) => b[1] - a[1]);
 
-  for (let i = 0; i < stages.length; i++) {
-    const level = stages[i];
-
-    for (let j = 0; j < level; j++) {
-      players[j].reached++;
-    }
-    players[level - 1].solved++;
-  }
-
-  for (let i = 0; i < N + 1; i++) {
-    players[i].failRate = players[i].reached !== 0 ? players[i].solved / players[i].reached : 0;
-  }
-
-  // (N + 1) level의 경우 마지막 스테이지까지 클리어 한 사용자를 나타낼 뿐 유효한 스테이지가 아니므로 삭제한다.
-  players.pop();
-
-  players.sort((a, b) => {
-    // 실패율 내림차순 정렬
-    if (a.failRate > b.failRate) {
-      return b.failRate - a.failRate;
-    }
-    // 실패율이 같은 경우 스테이지 번호 오름차순 정렬
-    if (a.failRate === b.failRate) {
-      return a.level - b.level;
-    }
-  })
-
-  for (let i = 0; i < players.length; i++) {
-    answer.push(players[i].level);
-  }
-
-  return answer;
+  // 스테이지 번호 배열 반환
+  return info.map(value => value[0]);
 }
