455d2be739a1111c2f50e59e9bec80772de6766e
sieukim <s2lver@naver.com>
2022-05-09 23:35:08 +0900

    [level 2] 멀쩡한 사각형(javascript)

--- level2/ex14.js
+++ level2/ex14.js
@@ -1,19 +1,29 @@
 function solution(w, h) {
-  let count = 0;
-
-  // 직사각형 대각선을 나타내는 일차 함수
-  // f(x) = (h / w) * x 인데
-  // h / w 계산에서 소수점이 부정확할 수 있어
-  // h * x 먼저하고 w로 나눔
-  const fx = (x) => h * x / w;
-
-  for (let i = 0; i < w; i++) {
-    const prev = Math.floor(fx(i));
-    const next = Math.ceil(fx(i + 1));
-    const diff = next - prev;
-
-    count += diff;
-  }
-
-  return w * h - count;
+  // 주어진 사각형이 정사각형인 경우
+ if (w === h) {
+     // 사용 불가능한 정사각형의 개수 = 한 변의 길이
+     return w * h - w
+ }
+ 
+ // 만약 한 변의 길이가 1인 경우
+ if (w == 1 || h == 1) {
+     // 사용 가능한 정사각형의 개수 = 0
+     return 0   
+ }
+ 
+ // 사용할 수 있는 정사각형의 개수
+ let count = 0;
+ 
+ // 직사각형의 대각선에 대응하는 함수 
+ const f = (x) => h * x / w
+ 
+ for (let i = 0; i < w; i++) {
+     // x좌표가 i일 때 y좌표와 (i+1)일 때 y좌표
+     const y = [Math.floor(f(i)), Math.ceil(f(i+1))];
+     
+     // 두 좌표 사이에 사용 불가능한 정사각형의 개수 = 두 y좌표의 차
+     count += y[1] - y[0]    
+ }
+ 
+ return w * h - count;
 }
