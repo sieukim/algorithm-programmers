7cd27633ca20faadfed43a7a03389e5b1440100a
sieukim <s2lver@naver.com>
2022-05-10 16:40:44 +0900

    [level 2] 오픈채팅방(javascript)

--- level2/ex13.js
+++ level2/ex13.js
@@ -1,45 +1,62 @@
-function solution(records) {
-  const answer = [];
-  const dict = {};
-
-  // 주어진 uid와 command를 가지고 문구로 변환하는 함수
-  function comment(uid, command) {
-    switch (command) {
-      case 'Enter':
-        return `${dict[uid].nickname}님이 들어왔습니다.`;
-      case 'Leave':
-        return `${dict[uid].nickname}님이 나갔습니다.`;
-    }
-  }
-
-  for (let i = 0; i < records.length; i++) {
-    // 주어진 기록을 공백을 기준으로 구분
-    const record = records[i].split(' ');
-
-    const command = record[0];
-    const uid = record[1];
-    const nickname = record[2];
-
-    // 이미 존재하는 경우
-    if (dict[uid]) {
-      // 닉네임 변경이 필요한 경우(Change 또는 재입장)
-      if (command !== 'Leave' && dict[uid].nickname !== nickname) {
-        dict[uid] = {
-          nickname: nickname,
-        }
+// 최종 닉네임 딕셔너리 반환 함수
+function get_nickname(n, record) {
+  // 닉네임 딕셔너리
+  const nickname = {};
+  
+  for (let i = 0; i < n; i++) {
+      // 종류 
+      const command = record[i][0];
+      
+      // 퇴장인 경우
+      if (command === 'Leave') {
+          // 건너뜀
+          continue;
       }
-    }
-    // 존재하지 않는 경우
-    else {
-      dict[uid] = {
-        nickname: nickname,
-      };
-    }
+      
+      // 아이디, 닉네임
+      const user_id = record[i][1];
+      const user_nickname = record[i][2];
 
-    if (command !== 'Change') {
-      answer.push([uid, command]);
-    }
+      // 현재 사용자의 닉네임 변경
+      nickname[user_id] = user_nickname;
   }
 
-  return answer.map(value => comment(value[0], value[1]));
+  return nickname
+}
+
+// 출력 메세지 리스트 반환 함수
+function get_messages(n, record, nickname) {
+  const messages = [];
+  
+  for (let i = 0; i < n; i++) {
+      // 종류, 아이디
+      const command = record[i][0];
+      const id = record[i][1];
+      
+      // 입장인 경우
+      if (command === 'Enter') {
+          messages.push(`${nickname[id]}님이 들어왔습니다.`);
+      }
+      
+      // 퇴장인 경우
+      if (command === 'Leave') {
+          messages.push(`${nickname[id]}님이 나갔습니다.`);
+      }
+  }
+      
+  return messages;
+}
+  
+function solution(record) {
+  // record 크기
+  const n = record.length;
+  // 각 문자열을 배열로 변환
+  record = record.map(value => value.split(' '));
+  
+  // 닉네임 딕셔너리
+  const nickname = get_nickname(n, record);
+  // 출력 메세지 리스트
+  const messages = get_messages(n, record, nickname);
+  
+  return messages
 }
