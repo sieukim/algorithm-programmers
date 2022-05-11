4cc754122e25d5a0f1c53c1e2f8f696948a157c9
sieukim <s2lver@naver.com>
2022-05-11 23:18:42 +0900

    [level 1] 소수 만들기(python)

--- level1/ex09.py
+++ level1/ex09.py
@@ -1,25 +1,24 @@
 from itertools import combinations
 
-def solution(nums):
-    # 소수 판별 함수
-    def isPrime(num):
-        if num <= 3:
-            return True
-        if num % 2 == 0:
-            return False
-        if num % 3 == 0:
-            return False
-        for i in range(4, int(num ** 0.5 + 1)):
-            if num % i == 0:
-                return False
+# 소수 판별 함수
+def isPrime(num):
+    if num < 2:
+        return False
+    if num < 4:
         return True
-        
+    if num % 2 == 0:
+        return False
+    if num % 3 == 0:
+        return False
+    for i in range(4, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def solution(nums):
     # 3개 요소로 이루어진 조합 리스트
-    comb = list(combinations(nums, 3))
-    # 각 조합의 합 리스트
-    combSums = list(map(lambda x: sum(x), comb))
-    # 중복 제거
-    combSums = list(set(combSums))
-    # 소수인 조합의 합 리스트
-    primes = [combSum for combSum in combSums if isPrime(combSum)]
-    return len(primes)
+    combs = list(combinations(nums, 3))
+    # 각 조합을 모든 요소의 합으로 변환
+    combs = [sum(comb) for comb in combs]
+    # 소수인 합의 개수 반환
+    return len([comb for comb in combs if isPrime(comb)])
