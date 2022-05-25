3c86cdeb984378d5ba60a4125d2f16421c08d22a
sieukim <s2lver@naver.com>
2022-05-25 22:20:09 +0900

    [level 2] 숫자의 표현(javascript)

--- level2/ex07.js
+++ level2/ex07.js
@@ -1,23 +1,25 @@
 function solution(n) {
-  let answer = 0;
-
-  // 주어진 정수부터 시작하여 연속하는 수를 더했을 때,
-  // 합이 n이 되는지를 반환하는 함수
-  function finn(i) {
-    let sum = 0;
-
-    while (i <= n && sum < n) {
-      sum += i;
-      i++;
-    }
-
-    if (sum === n) return true;
-    else return false;
-  }
-
+  // 표현 방법 수
+  let count = 0;
+  
   for (let i = 1; i <= n; i++) {
-    if (finn(i)) answer++;
+      // 연속된 자연수의 합
+      let sum = 0;
+      
+      for (let j = i; j <= n; j++) {
+          // 합 갱신 
+          sum += j;
+          // n이 표현된 경우
+          if (sum === n) {
+              count += 1;
+              break;
+          }
+          // n을 표현할 수 없는 경우
+          if (sum > n) {
+              break;
+          }        
+      }
   }
-
-  return answer;
+  
+  return count;
 }
