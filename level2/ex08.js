25b37583c98c30e6b7a9a5c07e26e2882ff98b40
sieukim <s2lver@naver.com>
2022-05-26 23:40:40 +0900

    [level 2] 다음 큰 숫자(javascript)

--- level2/ex08.js
+++ level2/ex08.js
@@ -1,54 +1,22 @@
 function solution(n) {
-  // 이진수 배열을 십진수로 변환하는 함수
-  function toDecimal(number) {
-    let answer = 0;
+  // 2진수로 변환하여 1의 개수를 반환하는 함수
+  function get_count(num) {
+      // 2진수로 변환 + 배열로 변환
+      const binary = num.toString(2).split('');
+      // 0의 개수 반환
+      return binary.filter(value => value === '1').length;
+  }
 
-    number.forEach((v, i) => {
-      if (v === 1) {
-        answer += Math.pow(2, number.length - 1 - i);
+  let answer = -1;
+  
+  for (let i = n + 1; i <= 1000000; i++) {
+      // 1의 개수가 같은 경우
+      if (get_count(n) === get_count(i)) {
+          answer = i;
+          // 종료
+          break;
       }
-    });
-
-    return answer;
   }
-
-  // 십진수를 이진수 배열로 변환하고 1의 개수를 반환하는 함수
-  function toBinary(number) {
-    const binary = [];
-    let count = 0;
-
-    while (number > 0) {
-      // 2로 나눈 나머지
-      const r = parseInt(number % 2);
-      // 2로 나눈 몫
-      const q = parseInt(number / 2);
-
-      // 나머지가 1이면 개수 증가
-      if (r === 1) count++;
-
-      // 2진수 배열에 나머지 추가
-      binary.push(r);
-
-      // number 갱신
-      number = q;
-    }
-
-    return [binary.reverse(), count];
-  }
-
-  let binary = [];
-
-  const [_, count] = toBinary(n);
-
-  // 주어진 n보다 크면서 1의 개수가 같은 최소의 수 찾기
-  while (true) {
-    const [newBinary, newCount] = toBinary(++n);
-
-    if (newCount === count) {
-      binary = newBinary;
-      break;
-    }
-  }
-
-  return toDecimal(binary);
+  
+  return answer;
 }
