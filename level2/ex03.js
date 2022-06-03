3982f9593ded6213b621bac8bf509dc23cece426
sieukim <s2lver@naver.com>
2022-06-03 22:02:51 +0900

    [level 2] 행렬의 곱셈(javascript)

--- level2/ex03.js
+++ level2/ex03.js
@@ -1,42 +1,32 @@
-function solution(arr1, arr2) {
-  // 행렬 곱 결과: m x n 행렬
-  const m = arr1.length;
-  const n = arr2[0].length;
-  const answer = Array.from(new Array(m), () => new Array(n));
-
-  // j 번째 열의 모든 요소를 갖는 배열을 반환하는 함수
-  function findColumn(arr, j) {
-    const answer = [];
-    const m = arr.length;
-    const n = arr[0].length;
-
-    for (let i = 0; i < m; i++) {
-      answer.push(arr[i][j]);
-    }
-
-    return answer;
+function solution(arr1, arr2) {    
+  // 크기가 같은 두 일차원 배열의 동일한 위치의 
+  // 요소를 모두 곱하고 더한 값을 반환하는 함수 
+  function get_sum(x, y) {
+      let sum = 0;
+      
+      for (let i = 0; i < x.length; i++) {
+          sum += x[i] * y[i];
+      }
+      
+      return sum;
   }
-
-  // 크기가 n x 1로 같은 두 배열의 곱을 구하는 함수
-  function multiply(row, column) {
-    let answer = 0;
-    const n = row.length;
-
-    for (let i = 0; i < n; i++) {
-      answer += row[i] * column[i];
-    }
-
-    return answer;
+  
+  // 결과 행렬의 크기 n x m
+  const n = arr1.length;
+  const m = arr2[0].length;
+  // 결과 행렬
+  const result = Array.from(new Array(n), () => new Array(m).fill(0));
+  
+  for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+          // arr1의 i번째 행, 크기 k
+          const x = arr1[i]
+          // arr2의 j번째 열, 크기 k
+          const y = arr2.map(value => value[j]);
+          // result[i][j]: x와 y의 각 요소의 곱을 합한 값
+          result[i][j] = get_sum(x, y);
+      }
   }
-
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      const row = arr1[i];
-      const column = findColumn(arr2, j);
-
-      answer[i][j] = multiply(row, column);
-    }
-  }
-
-  return answer;
+  
+  return result;
 }
