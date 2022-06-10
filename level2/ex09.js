cc37c78672bd691dbca2942257a96c7a29f6461a
sieukim <s2lver@naver.com>
2022-06-10 23:56:36 +0900

    [level 2] 올바른 괄호(javascript)

--- level2/ex09.js
+++ level2/ex09.js
@@ -1,14 +1,25 @@
 function solution(s){
-  // 문자열의 길이가 홀수인 경우 false를 반환
-  if (s.length % 2 === 1) return false;
-
+  // 여는 괄호의 개수
+  const n = s.split('').filter(value => value === '(').length;
+  // 닫는 괄호의 개수
+  const m = s.split('').filter(value => value === ')').length;
+  
+  // 두 괄호의 길이가 다른 경우
+  if (n !== m) {
+      return false;
+  }
+  
   const stack = [];
-
+  
   // 문자열을 탐색하며 (이 나오면 스택에 push 아니면 pop
-  s.split('').forEach(v => {
-    v === '(' ? stack.push(v) : stack.pop();
-  })
-
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === '(') {
+          stack.push(s[i]);
+      } else {
+          stack.pop();
+      }
+  }
+  
   // stack에 남은 요소가 있으면 false를, 없으면 true를 반환
-  return stack.length > 0 ? false : true;
+  return stack.length === 0;
 }
