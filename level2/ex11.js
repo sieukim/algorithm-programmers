f2ef2cda7aac53abc7342ce241652c1ef5f5d1b5
sieukim <s2lver@naver.com>
2022-06-11 20:56:42 +0900

    [level 2] 위장(javascript)

--- level2/ex11.js
+++ level2/ex11.js
@@ -1,26 +1,26 @@
 function solution(infos) {
-  const clothes = {};
-
-  // 주어진 정보를 의상종류: [...의상 이름]의 형태로 변환
-  infos.forEach(info => {
-    const type = info[1];
-    const cloth = info[0];
-
-    // 해당 종류가 처음 발견된 경우
-    if (!Object.keys(clothes).includes(type)) clothes[type] = 1;
-    else clothes[type]++;
-  });
-
-  // 조합 수 계산
-  const values = Object.values(clothes);
-
-  let count = 1;
-
-  // (가지수 + 1)씩 곱하여 총 조합의 수 계산
-  for (let i = 0; i < values.length; i++) {
-    count = count * (values[i] + 1);
+  // {의상 종류: [의상 수]}
+  const obj = {};
+  
+  for (const info of infos) {
+      // 의상 이름, 의상 종류
+      const [_, type] = info;
+      
+      // 의상 종류가 존재하는 경우
+      if (obj[type]) {
+          // 의상 수 추가 
+          obj[type] += 1;
+      }
+      // 의상 종류가 존재하지 않는 경우
+      else {
+          // 의상 종류 추가
+          obj[type] = 1;
+      }
   }
-
-  // 한 벌도 안 입는 경우는 제외
-  return count - 1;
+  
+  // 조합의 수
+  const count = Object.values(obj).reduce((a, b) => a * (b + 1), 1);
+  
+  // 0개 조합은 제외 
+  return count - 1; 
 }
