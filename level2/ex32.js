a08736c8cc2af9554b84a2d3161432b8a8de7e0c
sieukim <s2lver@naver.com>
2022-06-20 04:17:19 +0900

    [level 2] 영어 끝말잇기(javascript)

--- level2/ex32.js
+++ level2/ex32.js
@@ -1,35 +1,22 @@
 function solution(n, words) {
-  // 현재 인덱스를 이용하여 [번호, 차례]를 반환하는 함수
-  function getAnswer(n, index) {
-    const player = index % n + 1;
-    const turn = parseInt(index / n) + 1;
-
-    return [player, turn];
-  }
-
-  // 정답 배열
   const answer = [];
-
+  
   // 이전 단어의 마지막 글자
-  let lastCharacter;
+  let prev = words[0][0];
+  
+  for (let i = 1; i < words.length; i++) {
+      // 이전 단어
+      const prev = words[i - 1];
+      // 현재 단어
+      const curr = words[i];
 
-  for (let i = 0; i < words.length; i++) {
-    const word = words[i];
-
-    // lastCharacter로 시작하지 않는 경우
-    if (lastCharacter && lastCharacter !== word[0]) {
-      return getAnswer(n, i);
-    }
-
-    // lastCharacter 갱신
-    lastCharacter = word[word.length - 1];
-
-    // 중복 단어인 경우
-    if (words.indexOf(word) !== i) {
-      return getAnswer(n, i);
-    }
-
+      // 탈락
+      if (words.slice(0, i).includes(curr) || prev.slice(-1) !== curr[0]) {
+          answer.push(i % n + 1);
+          answer.push(Math.ceil((i + 1) / n));
+          break;
+      }
   }
 
-  return [0, 0];
+  return answer.length === 0 ? [0, 0] : answer;
 }
