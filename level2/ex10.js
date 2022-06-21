b0266ead654e8c4f290a898c464fa42007dc2f47
sieukim <s2lver@naver.com>
2022-06-22 02:37:27 +0900

    [level 2] 캐시(javascript)

--- level2/ex10.js
+++ level2/ex10.js
@@ -1,35 +1,44 @@
-function solution(cacheSize, cities) {
-  if (cacheSize === 0) return 5 * cities.length;
-
-  // 총 실행 시간
-  let answer = 0;
-
-  // 캐시
-  const cache = {};
-
-  for (let i = 0; i < cities.length; i++) {
-    const keys = Object.keys(cache);
-    const values = Object.values(cache).sort((a, b) => a - b);
-    const city = cities[i].toLowerCase();
-
-    // cache hit
-    if (keys.includes(city)) {
-      // 실행 순서 갱신
-      delete cache[city];
-      cache[city] = i;
-      answer += 1;
-    }
-    // cache miss
-    else {
-      // 빈 자리가 없는 경우
-      if (keys.length === cacheSize) {
-        delete cache[keys[0]];
-      }
-
-      cache[city] = i;
-      answer += 5;
-    }
+// 캐시 삽입 함수
+function add(cache, value, size) {
+  // 빈 자리가 있는 경우
+  if (cache.length < size) {
+      cache.push(value);
   }
+  // 빈 자리가 없는 경우
+  else if (size > 0) {
+      // 이미 삽입된 경우
+      if (cache.includes(value)) {
+          cache = cache.filter(item => item !== value);
+      }
+      // 새로 삽입하는 경우
+      else {
+          cache = cache.slice(1);
+      }
+      cache.push(value);
+  }
+  return cache;
+}
 
+function solution(cacheSize, cities) {
+  // 실행 시간
+  let answer = 0;
+  // 캐시
+  let cache = [];
+  // 모두 소문자로 변환
+  cities = cities.map(city => city.toLowerCase());
+  
+  for (const city of cities) {
+      // 캐시 hit
+      if (cache.includes(city)) {
+          answer += 1;
+      }
+      // 캐시 miss
+      else {
+          answer += 5;
+      }
+      // 캐시 삽입
+      add(cache, city, cacheSize);
+  }
+  
   return answer;
 }
