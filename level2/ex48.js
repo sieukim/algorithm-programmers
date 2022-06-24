d94ec098f4e8221c5dd1987e5d5009b11c461e36
sieukim <s2lver@naver.com>
2022-06-25 03:41:32 +0900

    [level 2] 모음 사전(javascript)

--- level2/ex48.js
+++ level2/ex48.js
@@ -1,36 +1,35 @@
 function solution(word) {
-  // 알파벳 중복순열 구하기
-  function getPermutations(arr, n, r) {
-    // r이 1인 경우
-    if (r === 1) {
-      return arr.map(value => [value]);
-    }
-
-    const permutations = [];
-
-    for (let i = 0; i < n; i++) {
-      const subPermutations = getPermutations(arr, n, r - 1);
-      const currentPermutations = subPermutations.map(perm => [arr[i], ...perm]);
-      permutations.push(...currentPermutations);
-    }
-    return permutations;
+  // 중복 순열 
+  function getPerms(arr, r) {
+      if (r === 1) {
+          return arr.map(value => [value]);
+      }
+      
+      const perms = [];
+      
+      for (let i = 0; i < arr.length; i++) {
+          const subPerms = getPerms(arr, r - 1);
+          const curPerms = subPerms.map(perm => [arr[i], ...perm]);
+          perms.push(...curPerms);
+      }
+      
+      return perms;
   }
-
+  
+  // 모음 리스트 
   const arr = ['A', 'E', 'I', 'O', 'U'];
-  const n = arr.length;
-
-  const totalPermutations = [];
-
-  // nPi1 ~ nPin 구하기
-  for (let i = 1; i <= n; i++) {
-    const r = i;
-    // 중복 순열을 구해 문자열로 저장
-    const permutations = getPermutations(arr, n, r).map(perm => perm.join(''));
-    totalPermutations.push(...permutations);
+  
+  // 단어 리스트 
+  const words = [];
+  
+  // 중복 순열
+  for (let i = 1; i <= arr.length; i++) {
+      const perms = getPerms(arr, i).map(perm => perm.join(''));
+      words.push(...perms);
   }
+  
+  // 오름차순 정렬 
+  words.sort();
 
-  // 사전순 정렬
-  totalPermutations.sort();
-
-  return totalPermutations.indexOf(word) + 1;
+  return words.indexOf(word) + 1;
 }
