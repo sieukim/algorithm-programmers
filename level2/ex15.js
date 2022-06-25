cd2a04d437f358e57ac64126fcae6a2722c880ac
sieukim <s2lver@naver.com>
2022-06-26 04:38:44 +0900

    [level 2] 짝지어 제거하기(javascript)

--- level2/ex15.js
+++ level2/ex15.js
@@ -1,15 +1,22 @@
 function solution(s) {
+  // 길이가 홀수 => 절대 불가능
+  if (s.length % 2 === 1) {
+      return 0;
+  }
+  
+  // 스택
   const stack = [];
-
-  s.split('').forEach(alpha => {
-    const popped = stack.pop();
-
-    // 연속되지 않는 경우
-    if (popped !== alpha){
-      if (popped) stack.push(popped);
-      stack.push(alpha);
-    }
-  });
-
+  
+  for (let i = 0; i < s.length; i++) {
+      // 짝인 경우
+      if (stack[stack.length - 1] === s[i]) {
+          stack.pop();
+      }
+      // 짝이 아닌 경우
+      else {
+          stack.push(s[i]);
+      }
+  }
+  
   return stack.length === 0 ? 1 : 0;
 }
