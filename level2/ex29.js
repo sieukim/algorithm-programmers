ea3ef9007b26999eea0dc773dc4a0a7fa7c90910
sieukim <s2lver@naver.com>
2022-06-28 22:42:11 +0900

    [level 2] 타겟 넘버(javascript)

--- level2/ex29.js
+++ level2/ex29.js
@@ -1,28 +1,22 @@
-function solution(numbers, target) {
-  let answer = 0;
-
-  // 주어진 정수 집합 오름차순 정렬
-  numbers.sort((a, b) => a - b);
-
-  // dfs
-  const dfs = (sum, index, arr) => {
-    // 모든 수에 대한 연산이 결정되었을 때
-    if (index === numbers.length) {
-      // 타겟 넘버를 만든 경우
-      if (sum === target) {
-        answer++;
+function dfs(numbers, target, index = 0, result = 0, answer = 0) {
+  // 종료
+  if (index === numbers.length) {
+      // 타겟 넘버 생성 여부
+      if (result === target) {
+          return 1;
+      } else {
+          return 0;
       }
-      return;
-    }
-
-    // 현재 인덱스 요소를 합에서 빼서 dfs 실행
-    dfs(sum - numbers[index], index + 1, [...arr, '-']);
-    // 현재 인덱스 요소를 합에서 더해서 dfs 실행
-    dfs(sum + numbers[index], index + 1, [...arr, '+']);
   }
-
-  // dfs 실행
-  dfs(0, 0, []);
-
+  
+  // 더하기
+  answer += dfs(numbers, target, index + 1, result + numbers[index]);
+  // 빼기
+  answer += dfs(numbers, target, index + 1, result - numbers[index]);
+  
   return answer;
+}
+
+function solution(numbers, target) {
+  return dfs(numbers, target);
 }
