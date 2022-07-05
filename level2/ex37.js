571af4c2cba5f27ff31b0e5a1e8a83a51cc6bbaa
sieukim <s2lver@naver.com>
2022-07-05 19:24:35 +0900

    [level 2] 압축(javascript)

--- level2/ex37.js
+++ level2/ex37.js
@@ -1,41 +1,43 @@
-function solution(msg) {
-  // 출력 색인번호 배열
-  const answer = [];
+// 사전에 포함되는 가장 긴 문자열 w를 찾는 함수 
+function find(words, msg) {
+  for (let i = msg.length; i > 0; i--) {
+      const sub_msg = msg.slice(0, i);
 
-  // 사전
-  const dict = {};
-
-  // 길이가 1인 모든 단어를 포함하도록 사전을 초기화
-  for (let i = 0; i < 26; i++) {
-    // 아스키 코드 -> 문자 변환
-    const charCode = String.fromCharCode(65 + i);
-    // dict 초기화
-    dict[`${charCode}`] = i + 1;
-  }
-
-  for (let i = 0; i < msg.length; i++) {
-    // 사전에서 현재 입력과 일치하는 가장 긴 문자열 w
-    let w = '';
-    // for문 순회 인덱스
-    let j = i;
-
-    for (j = i; j < msg.length; j++) {
-      // 사전에 포함하는 경우
-      if (dict[`${w + msg[j]}`]) {
-        w += msg[j];
-        continue;
+      if (sub_msg in words) {
+          return [sub_msg, i];
       }
-      // 사전에 포함하지 않는 경우
-      break;
-    }
-
-    // w에 해당하는 사전의 색인 번호를 출력
-    answer.push(dict[`${w}`]);
-    // w + 다음글자를 사전에 등록
-    dict[`${w + msg[j]}`] = Object.keys(dict).length + 1;
-    // i 갱신
-    i += w.length - 1;
-
   }
-  return answer;
+}
+
+// LZW 압축 알고리즘 
+function LZW(msg, index=[]) {
+  // 사전 초기화 
+  const dict = {};
+  let dictSize = 27;
+  
+  for (let i = 0; i < dictSize; i++) {
+      const char = String.fromCharCode(65 + i);
+      dict[char] = i + 1;
+  }
+  
+  while (msg.length > 0) {
+      const [w, i] = find(dict, msg);
+
+      // 인덱스 출력
+      index.push(dict[w]);
+      
+      // 사전에 등록
+      if (i < msg.length) {
+          dict[w + msg[i]] = dictSize++;
+      }
+      
+      // 입력 갱신
+      msg = msg.slice(i);
+  }
+  
+  return index;
+}
+
+function solution(msg) {
+  return LZW(msg);
 }
