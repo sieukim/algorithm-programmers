e5998e6b52e69c2dce177e96889cac4dbc7ff91f
sieukim <s2lver@naver.com>
2022-07-07 19:16:01 +0900

    [level 2] 이진 변환 반복하기(javascript)

--- level2/ex16.js
+++ level2/ex16.js
@@ -1,32 +1,16 @@
+// 이진 변환 함수
+function convert(s, times=0, count=0) {
+  if (s === '1') {
+      return [times, count];
+  }
+  
+  const n = s.length;
+  const m = s.split('').filter(value => value === '0').length;
+  s = (n - m).toString(2);
+  
+  return convert(s, times + 1, count + m);
+}
+
 function solution(s) {
-  // 1. 주어진 문자열에서 모든 0을 제거
-  function step1(str) {
-    return str.split('').filter(value => {
-      if (value === '0') zeroCount++;
-      return value === '1';
-    }).join('');
-  }
-
-  // 2. 주어진 숫자를 2진법으로 변환
-  function step2(n) {
-    const binary = [];
-
-    while (n > 0) {
-      binary.push(n % 2);
-      n = parseInt(n / 2);
-    }
-
-    return binary.reverse().join('');
-  }
-
-  let count = 0;
-  let zeroCount = 0;
-
-  while (s !== '1') {
-    s = step1(s);
-    s = step2(s.length);
-    count++;
-  }
-
-  return [count, zeroCount];
+  return convert(s);
 }
