a51babaeef16e4dba401e58e12ce1c31150854ad
sieukim <s2lver@naver.com>
2022-07-09 17:46:23 +0900

    [level 2] 방금 그 곡(javascript)

--- level2/ex40.js
+++ level2/ex40.js
@@ -1,94 +1,66 @@
+// 재생 시간 반환 함수
+function getTime(time1, time2) {
+  const [hour1, minute1] = time1.split(':').map(value => parseInt(value));
+  const [hour2, minute2] = time2.split(':').map(value => parseInt(value));
+  return (hour2 - hour1) * 60 + (minute2 - minute1);
+}
+
+// 재생 악보 반환 함수 
+function getMelody(melody, times) {
+  melody = replace(melody);
+  const div = parseInt(times / melody.length);
+  const mod = times % melody.length;
+  
+  if (div === 0) {
+      return melody.slice(0, mod);
+  } else {
+      return melody.repeat(div) + melody.slice(0, mod);
+  }
+}
+
+// 문자열 -> 딕셔너리
+function convert(info, index) {
+  info = info.split(',');
+  
+  const title = info[2];
+  const times = getTime(info[0], info[1]);
+  const melody = getMelody(info[3], times);
+  
+  return {title, times, melody, index};
+}
+
+// C# -> c, D# -> d, F# -> f, G# -> g, A# -> a
+function replace(melody) {
+  return melody.replace(/C#/g, 'c')
+               .replace(/D#/g, 'd')
+               .replace(/F#/g, 'f')
+               .replace(/G#/g, 'g')
+               .replace(/A#/g, 'a')
+}
+
+// 필터링 함수
+function filter(m, musicinfos) {
+  m = replace(m);
+  musicinfos = musicinfos.filter(value => value.melody.includes(m));
+  return musicinfos;
+}
+
+// 정렬 함수
+function sort(musicinfos) {
+  musicinfos.sort((a, b) => {
+      if (a.times === b.times) {
+          return a.index - b.index;     
+      } else {
+          return b.times - a.times;
+      }
+  });
+  
+  return musicinfos;
+}
+
 function solution(m, musicinfos) {
-  // 주어진 악보 정보를 배열로 반환하는 함수
-  function getMusicNote(music) {
-    const musicScore = [];
-
-    for (let i = 0; i < music.length; i++) {
-      // #이 붙는 경우
-      if (music[i + 1] === '#') {
-        musicScore.push(music[i] + music[i + 1]);
-        i++;
-      }
-      // #이 안 붙는 경우
-      else {
-        musicScore.push(music[i] + '@');
-      }
-    }
-
-    return musicScore;
-  }
-
-  // 주어진 문자열을 이용하여 음악 정보를 반환하는 함수
-  function getMusicInfo(str) {
-    // infoArr = [시간 정보, 제목, 악보]
-    const infoArr = str.split(',');
-
-    // 음악 정보 객체
-    const musicinfo = {};
-
-    // 시작 시간
-    const [startTimeHour, startTimeMinute] = infoArr[0].split(':');
-    // 종료 시간
-    const [endTimeHour, endTimeMinute] = infoArr[1].split(':');
-
-    // 시간 정보
-    const startTime = new Date(2022, 3, 19, startTimeHour, startTimeMinute);
-    const endTime = new Date(2022, 3, 19, endTimeHour, endTimeMinute);
-
-    // 재생 시간
-    let playTime = (endTime.getTime() - startTime.getTime()) / 60000;
-
-    // 00:00시에 끝나는 경우
-    if (playTime < 0) playTime += 1440;
-
-    musicinfo.playTime = playTime;
-
-    // 제목
-    const title = infoArr[2];
-    musicinfo.title = title;
-
-    // 악보
-    const musicNote = getMusicNote(infoArr[3]);
-
-    // 재생 시간에 맞게 반복한 악보
-    const replayedMusicNote = [];
-    // 재생 횟수
-    const replayCount = Math.floor(musicinfo.playTime / musicNote.length);
-    // 남은 멜로디 개수
-    const extraCount = musicinfo.playTime % musicNote.length;
-
-    for (let i = 0; i < replayCount; i++) {
-      replayedMusicNote.push(...musicNote);
-    }
-
-    for (let i = 0; i < extraCount; i++) {
-      replayedMusicNote.push(musicNote[i]);
-    }
-
-    musicinfo.musicNote = replayedMusicNote.join(' ');
-
-    return musicinfo;
-  }
-
-  // 주어진 멜로디 조건을 만족하는 음악의 제목을 반환하는 함수
-  function getTitle(m, musicinfos) {
-    for (const musicinfo of musicinfos) {
-      const musicNote = musicinfo.musicNote;
-      // 멜로디 조건을 만족하는 경우
-      if (musicNote.includes(m)) return musicinfo.title;
-    }
-    return '(None)';
-  }
-
-  // 악보 정보 배열로 변환 후 공백 기준으로 합치기
-  m = getMusicNote(m).join(' ');
-  // 음악 정보 객체로 변환
-  musicinfos = musicinfos.map(musicinfo => getMusicInfo(musicinfo));
-  // 재생 시간 기준으로 내림차순 정렬
-  musicinfos.sort((a, b) => b.playTime - a.playTime);
-
-  // 멜로디 조건을 만족하는 음악 제목
-  const title = getTitle(m, musicinfos);
-
-  return title;
+  musicinfos = musicinfos.map((value, index) => convert(value, index));
+  musicinfos = filter(m, musicinfos);
+  musicinfos = sort(musicinfos);
+  return musicinfos.length > 0 ? musicinfos[0]['title'] : '(None)';
 }
