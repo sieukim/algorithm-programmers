e6c43dd706d2993ebe3427a7cad2dab2ac9473e3
sieukim <watasieun@gmail.com>
2022-08-03 18:26:10 +0700

    [level3] 가장 먼 노드(javascript)

--- level3/ex01.js
+++ level3/ex01.js
@@ -1,71 +1,36 @@
 function solution(n, vertex) {
-  // 주어진 간선 배열 정보를 이용하여 노드 객체 초기화하고 반환하는 함수
-  function getNodes(n, vertex) {
-    // 노드 객체 선언
-    const nodes = {};
-
-    // 노드 객체 초기화 ({노드 번호: [인접 노드 배열]} 형태)
-    for (let i = 1; i <= n; i++) {
-      nodes[i] = [];
-    }
-
-    // 각 노드의 인접 노드 배열 초기화
-    for (const v of vertex) {
-      const [node1, node2] = v;
-
-      nodes[node1].push(node2);
-      nodes[node2].push(node1);
-    }
-
-    return nodes;
+  // {노드: [인접 노드]}
+  const graph = {};
+  
+  for (let i = 0; i < vertex.length; i++) {
+      const [a, b] = vertex[i];
+      if (graph[a]) graph[a].push(b);
+      else graph[a] = [b];
+      if (graph[b]) graph[b].push(a);
+      else graph[b] = [a];
   }
-
-  // 주어진 노드 객체 정보를 이용하여 각 노드의 level 정보를 담은 배열과 최대 level을 반환하는 함수
-  function getLevels(n, nodes) {
-    // queue 초기화
-    const queue = [];
-    // 노드 방문 여부 정보를 담은 배열
-    // visited[i] = -1 && 방문하지 않은 경우
-    // visited[i] = i번 노드의 레벨 && 방문한 경우
-    let visited = new Array(n + 1).fill(-1);
-
-    // 1번 노드에 대한 초기화
-    queue.push(1);
-    visited[1] = 1;
-
-    // 노드 객체에 대한 bfs
-    while (queue.length > 0) {
-      // 현재 노드
-      const currentNode = queue.shift();
-      // 현재 노드의 인접 배열
-      const arr = nodes[currentNode];
-
-      // 인접 배열 탐색
-      arr.forEach(node => {
-        // 방문하지 않은 경우
-        if (visited[node] < 0) {
-          // queue에 추가
-          queue.push(node);
-          // 해당 노드의 레벨 갱신
-          visited[node] = visited[currentNode] + 1;
-        }
-      });
-    }
-
-    return [visited, visited.reduce((a, b) => Math.max(a, b))];
+  
+  function bfs(x, q=[], level=Array(n+1).fill(-1)) {
+      q.push(x);
+      level[x] = 0;
+      
+      while (q.length > 0) {
+          const y = q.shift();
+          for (const z of graph[y]) {
+              if (level[z] === -1) {
+                  q.push(z);
+                  level[z] = level[y] + 1;
+              }
+          }
+      }
+      
+      return level;
   }
-
-  // 노드 객체 초기화
-  const nodes = getNodes(n, vertex);
-  // level 배열 초기화
-  const [levels, maxLevel] = getLevels(n, nodes);
-
-  // 최대 level 노드 개수
-  let count = 0;
-
-  levels.forEach(level => {
-    if (level === maxLevel) count++;
-  });
-
-  return count;
+  
+  // level[i]: 1번 노드부터 i번 노드까지 거리
+  const level = bfs(1);
+  
+  // 가장 멀리 떨어진 노드의 개수 반환
+  const max_level = level.reduce((a, b) => Math.max(a, b));
+  return level.filter(v => v === max_level).length;
 }
