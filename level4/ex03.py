5f4fc07c4bc6595f43d8ea4272ee77f3c318f91e
sieukim <s2lver@naver.com>
2022-10-09 22:50:16 +0900

    [level 4] 가사 검색(python)

--- level4/ex03.py
+++ level4/ex03.py
@@ -1,38 +1,54 @@
 from collections import defaultdict
 
-def solution(words, queries, prefix=0, suffix=1):
-    # 쿼리 -> 와일드카드 문자열의 위치, 알파벳 문자열, 와일드카드 문자열
-    def split(query): 
-        # 와일드카드 개수
-        n = query.count('?') 
-        # 와일드카드 문자열의 위치
-        type = suffix if query[-1] == '?' else prefix 
-        # 알파벳 문자열
-        alphabet = query[n:] if type == prefix else query[:-n] 
-        # 와일드카드 문자열
-        wildcard = query[:n] if type == prefix else query[-n:] 
-        # 정보 반환
-        return type, alphabet, wildcard
+class Node(object):
+    def __init__(self, key):
+        self.key = key # 노드 정보
+        self.keys = defaultdict(int) # 해당 노드를 거치는 문자열 정보 {길이: 개수}
+        self.data = None # 해당 노드를 터미널 노드로 같은 문자열
+        self.children = {} # 자식 노드 딕셔너리
         
-    # 쿼리와 일치하는 단어의 개수를 반환하는 함수
-    def match(words, query, count=0):
-        # 와일드카드 문자열의 위치, 알파벳 문자열, 와일드카드 문자열
-        type, alphabet, wildcard = split(query) 
-        # 알파벳 문자열과 와일드카드 문자열의 길이
-        n, m = len(alphabet), len(wildcard)
-        # 쿼리 길이를 기준으로 필터링
-        words = [word for word in words if len(word) == len(query)] 
-        # 쿼리 일치 여부 확인
-        if type == prefix:
-            words = [word for word in words if word[-n:] == alphabet] 
+class Trie(object):
+    def __init__(self):
+        self.head = Node(None)
+        
+    # 문자열 삽입
+    def insert(self, data):
+        node = self.head
+        
+        for key in data:
+            if key not in node.children:
+                node.children[key] = Node(key)
+            node.keys[len(data)] += 1
+            node = node.children[key]
+            
+        node.data = data
+        
+    # 문자열 탐색
+    def search(self, data, n):
+        node = self.head
+        
+        for key in data:
+            if key not in node.children:
+                return 0
+            node = node.children[key]
+        
+        return node.keys[n]
+
+def solution(words, queries):
+    result = []
+    pref_trie = Trie() # 와일드카드가 앞에 위치
+    post_trie = Trie() # 와일드카드가 뒤에 위치
+    
+    for word in words:
+        pref_trie.insert(word)
+        post_trie.insert(word[::-1])
+    
+    for query in queries:
+        data, n = query.strip('?'), len(query)
+        if query[-1] == '?':
+            count = pref_trie.search(data, n)
         else:
-            words = [word for word in words if word[:n] == alphabet]
-        # 개수 반환
-        return len(words)
+            count = post_trie.search(data[::-1], n)
+        result.append(count)
     
-    queries_dict = defaultdict(int)
-    
-    for query in set(queries):
-        queries_dict[query] = match(words, query)
-    
-    return [queries_dict[query] for query in queries]
+    return result
