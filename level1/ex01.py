d70f36aa4638345d8ce80f1f7d9afeaecc65e562
sieukim <s2lver@naver.com>
2023-02-23 21:23:11 +0900

    [level 1] 로또의 최고 순위와 최저 순위(python)

--- level1/ex01.py
+++ level1/ex01.py
@@ -1,17 +1,11 @@
 def solution(lottos, win_nums):
-    # 0의  개수
-    zero_count = lottos.count(0)
-    # 정답 개수
-    win_count = len([lotto for lotto in lottos if lotto in win_nums])
+    # 알아볼 수 없는 번호 개수
+    unknown = lottos.count(0)
+    # 일치하는 번호 개수
+    matched = len(set(lottos) & set(win_nums))
+    # 최고 등수
+    high_rank = min(6, 7 - matched - unknown)
+    # 최저 등수
+    low_rank = min(6, 7 - matched)
 
-    # 최대 정답 개수 (0이 모두 정답인 경우)
-    max_count = win_count + zero_count
-    # 최소 정답 개수 (0이 모두 오답인 경우)
-    min_count = win_count
-    
-    # 최고 순위
-    max_rank = 7 - max_count if max_count >= 2 else 6
-    # 최저 순위
-    min_rank = 7 - min_count if min_count >= 2 else 6
-
-    return [max_rank, min_rank]
+    return [high_rank, low_rank]
