ff29287a3d28525cb6ecb5c634c7f2db8a5e7cb0
sieukim <s2lver@naver.com>
2023-03-06 17:45:37 +0900

    [level 2] 뉴스 클러스터링(python)

--- level2/ex38.py
+++ level2/ex38.py
@@ -1,41 +1,29 @@
-# 다중 집합 반환 함수 
-def get_multiset(string):
-    # 대문자로 변환
-    string = string.upper()
-    # 다중 집합 생성 
-    multiset = ["".join((string[i], string[i+1])) for i in range(len(string) - 1)]
-    # 공백, 숫자, 특수 문자 포함 요소 제거
-    multiset = [value for value in multiset if value.isalpha()]
-    
-    # 다중 집합 반환
-    return multiset
-
-# 다중 집합의 교집합과 합집합의 크기 반환 함수
-def get_size(multiset1, multiset2):    
-    # 교집합 크기, 합집합 크기
-    size1, size2 = 0, 0
-    # 두 집합 내 모든 요소
-    values = set(multiset1) | set(multiset2)
-    
-    for value in values:
-        # 각 집합 내 개수
-        count1, count2 = multiset1.count(value), multiset2.count(value)
-        # 크기 계산
-        size1 += min(count1, count2)
-        size2 += max(count1, count2)
-    
-    return size1, size2
+from math import floor
 
 def solution(str1, str2):
-    # 다중 집합
-    multiset1 = get_multiset(str1)
-    multiset2 = get_multiset(str2)
+    # 다중 집합 생성
+    def init(str1):
+        # 소문자 -> 대문자
+        str1 = str1.upper()
+        # 두글자씩 끊기
+        group1 = [str1[i]+str1[i+1] for i in range(len(str1)-1)]
+        # 공백이나 숫자, 특수 문자가 들어있는 단어 제거
+        return list(filter(lambda x: x.isalpha(), group1))
     
-    # 모두 공집합인 경우
-    if len(multiset1) == 0 and len(multiset2) == 0:
-        return 65536
+    # 자카드 유사도 계산
+    def calculate(set1, set2):
+        # 사용 단어 set
+        words = set(set1+set2)
+        # 교집합 크기
+        size1 = sum([min(set1.count(word), set2.count(word)) for word in words])
+        # 합집합 크기
+        size2 = sum([max(set1.count(word), set2.count(word)) for word in words])
+        # 유사도 반환
+        if words:
+            return size1/size2
+        else:
+            return 1
     
-    # 교집합과 합집합의 크기
-    size1, size2 = get_size(multiset1, multiset2)
-    
-    return size1 / size2 * 65536 // 1
+    set1 = init(str1)
+    set2 = init(str2)
+    return floor(calculate(set1, set2)*65536)
