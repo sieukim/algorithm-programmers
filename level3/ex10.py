0912458293d2e5c49a23f928f1953b48d9726a62
sieukim <s2lver@naver.com>
2023-03-06 22:21:38 +0900

    [level 3] 야근 지수(python)

--- level3/ex10.py
+++ level3/ex10.py
@@ -1,17 +1,14 @@
 from heapq import heapify, heappush, heappop
 
 def solution(n, works):
-    # max heap 생성 
-    works = [-value for value in works]
+    # 최대 힙 구성
+    works = [-work for work in works]
     heapify(works)
-    
-    for i in range(n):
-        # 최대값 pop
-        max_value = heappop(works)
-        # 남은 작업량이 없는 경우
-        if max_value == 0:
-            return 0
-        # 1을 더해 push
-        heappush(works, max_value+1)
 
-    return sum([value ** 2 for value in works])
+    # N시간 동안 작업
+    for _ in range(n):
+        max_value = min(0, heappop(works)+1)
+        heappush(works, max_value)
+
+    # 피로도 계산
+    return sum([work*work for work in works])
